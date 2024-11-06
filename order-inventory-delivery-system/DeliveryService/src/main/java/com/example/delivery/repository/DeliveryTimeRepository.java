0338dd2ab0b87f8ee2b85b62f99d2514e396b037
Gourab <43839450+gdspaces@users.noreply.github.com>
2024-11-06 20:13:36 +0530

    Delete order-inventory-delivery-system directory

--- order-inventory-delivery-system/DeliveryService/src/main/java/com/example/delivery/repository/DeliveryTimeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.delivery.repository;
-
-import com.example.delivery.entity.DeliveryTime;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DeliveryTimeRepository extends JpaRepository<DeliveryTime, String> {}
--- order-inventory-delivery-system/InventoryService/src/main/java/com/example/inventory/repository/InventoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.inventory.repository;
-
-import com.example.inventory.entity.Inventory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InventoryRepository extends JpaRepository<Inventory, String> {}
